5109b73730d97514ff7463e98bd5fa352a7f1c05
jsheppard1997 <60655306+jsheppard1997@users.noreply.github.com>
2020-10-29 20:04:09 +0000

    Update hotel_terminal.py
    adding comments

--- hotel_terminal.py
+++ hotel_terminal.py
@@ -7,6 +7,7 @@ root = tk.Tk()
 root.withdraw()
 
 
+#ensuring the uploaded file is a .csv
 while True:
     try:
         file_path = filedialog.askopenfilename()
@@ -19,7 +20,7 @@ while True:
         break
 
 
-
+#combing the three columns for date, month and year into one in the format yyyy-mm-dd
 bookings_df['arrival_date'] = pd.to_datetime(bookings_df[['arrival_date_year', 
                                                             'arrival_date_month', 
                                                             'arrival_date_day_of_month']].astype(str).sum(axis=1), 
@@ -28,16 +29,18 @@ bookings_df['arrival_date'] = pd.to_datetime(bookings_df[['arrival_date_year',
 
 
 
-
+#removing all cancelled bookings
 bookings_df = bookings_df[bookings_df['is_canceled'] == 0]
 
-bookings_df['total_days_stayed'] = pd.to_timedelta(bookings_df['stays_in_weekend_nights'] + bookings_df['stays_in_week_nights'], unit="D")
+#finding the total days stayed by adding together the weekdays and weekend nights stayed
+bookings_df['total_days_stayed'] = pd.to_timedelta(bookings_df['stays_in_weekend_nights'] +
+                                    bookings_df['stays_in_week_nights'], unit="D")
 
-bookings_df['leaving_date'] = bookings_df['arrival_date'] + bookings_df['total_days_stayed']
+#calculating the leaving date
+bookings_df['leaving_date'] = bookings_df['arrival_date'] + \
+                                bookings_df['total_days_stayed']
 
-print(bookings_df[['arrival_date', 'total_days_stayed','leaving_date', 'is_canceled']])
-
-
+#getting user input for date and ensuring in yyyy-mm-dd format
 while True:
     try:
         date = input("Enter date (YYYY-MM-DD):")
@@ -51,29 +54,29 @@ while True:
 
 
 
+#creating a mask that finds all customers who are checked-in to the hotels on the date queried
+#and all customers who are arriving within the next 7 days
+final_df_mask = ((bookings_df['arrival_date'] <= date) &
+                 (date <= bookings_df['leaving_date'])) | ((date <= bookings_df['arrival_date'])  &
+                 (bookings_df['arrival_date'] <= (date + timedelta(days=7)) ))
 
-final_df_mask = ((bookings_df['arrival_date'] <= date) & (date <= bookings_df['leaving_date'])) | ((date <= bookings_df['arrival_date'])  & (bookings_df['arrival_date'] <= (date + timedelta(days=7)) ))
-
+#filtering the df to only show the entries in the mask above
 final_df = bookings_df.loc[final_df_mask]
 
-final_df.to_csv("bookings_" + str(date.date()) + ".csv", index=False)
+#saving as .csv
+final_df.to_csv("bookings" + str(date.date()) + ".csv", index=False)
 
-print("A bookings dataframe for " + str(date.date()) + " has been saved as bookings_" + str(date.date()) + ".csv")
+#letting the user know the file has been saved
+print("A bookings dataframe for " + str(date.date()) + 
+        " has been saved as bookings" + str(date.date()) + ".csv")
 
+#picking just the adults, childen and babies and summing to find the total count of each column
 total_df = final_df[['adults', 'children', 'babies']].sum(axis=0) 
 
-total_df.to_csv("total_adults_children_babies_" + str(date.date()) + ".csv", index=False)
+#saving as .csv
+total_df.to_csv("guests" + str(date.date()) + ".csv", index=False)
 
-print("A guest dataframe for " + str(date.date()) + " has been saved as total_adults_children_babies_" + str(date.date()) + ".csv")
-
-
-
-
-
-
-
-
-
-
-
+#letting the user know the file has been saved
+print("A guest dataframe for " + str(date.date()) + 
+        "has been saved as guests" + str(date.date()) + ".csv")
 
